65930585ec437ded99073dd8f5fe03de070eb586
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:36:23 +0000

    Generate photos resource with draft:resource

---
+++ db/migrate/20200224163623_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.text :caption
+      t.string :image
+      t.integer :owner_id
+
+      t.timestamps
+    end
+  end
+end
