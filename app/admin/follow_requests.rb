9c4b080b47c794f3ef32ea5a070edf52f76243bc
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:37:53 +0000

    Whitelist columns in FollowRequest for ActiveAdmin

--- app/admin/follow_requests.rb
+++ app/admin/follow_requests.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register FollowRequest do
+
+ permit_params :status, :sender_id, :recipient_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
